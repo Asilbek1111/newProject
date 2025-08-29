6942bd62bd8b1d295d108fceb0a2781a10c1f3aa
Asilbek1111 <johnwickffa2003@gmail.com>
2025-08-29 17:04:35 +0500

    uchinchi versiya

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Car from './car.png'
 function App() {
   return (
     <div className="App">
      Birinchi github proyekt!
+     <img src={Car} alt="" />
     </div>
   );
 }
